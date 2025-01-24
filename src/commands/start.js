95aad79f20ebacd77c82dba13c06b29f5e16e872
yanagiragi <qaz90473@gmail.com>
2025-01-24 20:20:51 +0800

    feat: Support to send image in start command

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,5 +1,12 @@
 async function handler (msg, match, config, bot) {
-    return bot.ReplyMessage(msg, 'Bello, My Name is Hatsuki')
+    const title = 'Bello, My Name is Hatsuki'
+    const thumbnail = config['Start.WelcomeImage']
+    if (thumbnail) {
+        return bot.ReplyPhoto(msg, thumbnail, title)
+    }
+    else {
+        return bot.ReplyMessage(msg, title)
+    }
 }
 
 module.exports = {
