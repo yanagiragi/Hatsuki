da55c416bd8cb9a71c5ab93f28283216b2613d49
yanagiragi <qaz90473@gmail.com>
2025-05-21 20:46:04 +0800

    fix: Try fix permission issue when fetch image

--- src/Plugins/MeowMeowImage/fetch.js
+++ src/Plugins/MeowMeowImage/fetch.js
@@ -3,47 +3,54 @@ const cheerio = require('cheerio')
 const child_process = require('child_process')
 const fs = require('fs')
 
-const url = process.argv[2]
-const resp = await fetch(url)
-const content = await resp.text()
-const $ = cheerio.load(content)
-const images = $('.FnStickerList .FnImage span')
+async function Run () {
+    const url = process.argv[2]
+    const resp = await fetch(url)
+    const content = await resp.text()
+    const $ = cheerio.load(content)
+    const images = $('.FnStickerList .FnImage span')
 
-const folder = $('.mdCMN38Item0lHead').text().trim()
+    const folder = $('.mdCMN38Item0lHead').text().trim()
 
-const result = []
-for (let i = 0; i < images.length; ++i) {
-    const image = images[i]
-    const style = $(image).attr('style')
-    const src = style.replace('background-image:url(', '').replace(');', '')
-    const filename = `${i}.png`
+    const result = []
+    for (let i = 0; i < images.length; ++i) {
+        const image = images[i]
+        const style = $(image).attr('style')
+        const src = style.replace('background-image:url(', '').replace(');', '')
+        const filename = `${i}.png`
 
-    if (fs.existsSync(filename)) {
-        continue
+        if (fs.existsSync(filename)) {
+            continue
+        }
+
+        if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder)
+        }
+
+        let command = `curl ${src} -o '${folder}/${filename}'`
+        command += `; cp '${folder}/${filename}' "/tmp/${i}.png"`
+        command += `; ocr "/tmp/${i}.png"`
+
+        const rawOcrOutput = child_process.execSync(command)
+        try {
+            const ocrResult = JSON.parse(rawOcrOutput)
+            result.push({
+                filename,
+                text: ocrResult.text
+            })
+            console.log(`${filename} parsed. Result = ${ocrResult.text}`)
+        }
+        catch (err) {
+            result.push({
+                filename,
+                text: ''
+            })
+            console.log(`Unable to parse ${src}`)
+        }
     }
 
-    let command = `mkdir -p "${folder}"`
-    command += `; curl ${src} -o "${filename}"`
-    command += `; cp "${filename}" "/tmp/${i}.png"`
-    command += `; ocr "/tmp/${i}.png"`
-
-    const rawOcrOutput = child_process.execSync(command)
-    try {
-        const ocrResult = JSON.parse(rawOcrOutput)
-        result.push({
-            filename,
-            text: ocrResult.text
-        })
-        console.log(`${filename} parsed. Result = ${ocrResult.text}`)
-    }
-    catch (err) {
-        result.push({
-            filename,
-            text: ''
-        })
-        console.log(`Unable to parse ${src}`)
-    }
+    const output = JSON.stringify(result, null, 4)
+    fs.writeFileSync(`${folder}/result.json`, output)
 }
 
-const output = JSON.stringify(result, null, 4)
-fs.writeFileSync(`${folder}/result.json`, output)
+Run()
